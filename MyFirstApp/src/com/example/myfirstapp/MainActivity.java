35d133110b3d1ed22e7f15f61a83465d663452b8
bbassaka <bbassaka@VTEKL3BBASSAKA.telsim.local>
2013-02-09 16:40:37 +0200

    http://developer.android.com/training/basics/firstapp/starting-activity.html "Starting Activity" dersi

--- MyFirstApp/src/com/example/myfirstapp/MainActivity.java
+++ MyFirstApp/src/com/example/myfirstapp/MainActivity.java
@@ -1,11 +1,26 @@
 package com.example.myfirstapp;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
-
+	public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+	
+	public void sendMessage(View view){
+		//Do something in response to button
+    	Intent intent = new Intent(this, DisplayMessageActivity.class);
+    	// grab textbox value (locate by id)
+    	EditText editText = (EditText) findViewById(R.id.edit_message);
+    	String message = editText.getText().toString();
+    	// add to payload - first param is key
+    	intent.putExtra(EXTRA_MESSAGE, message);
+    	// start it
+    	startActivity(intent);
+	}
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
